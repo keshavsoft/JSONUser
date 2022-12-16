c2948e0221f142194d04eb2860db4cb14a31da4c
keshavsoft <nknnkumar@yahoo.com>
2022-12-16 18:57:50 +0530

    keshav

--- Projects/JSONUser/Dal/Users/Api/Setup.js
+++ Projects/JSONUser/Dal/Users/Api/Setup.js
@@ -3,7 +3,8 @@
 
 let CommonFromTemplate = require("../../../../../DataSupply/Fs/LoginFolder/Admin/Setup/FromTemplate");
 let CommonBasic = require("../../../../../DataSupply/Fs/LoginFolder/Admin/Setup/Basic");
-let CommonForMsSql = require("../../../../../DataSupply/Fs/LoginFolder/Admin/Setup/ForMsSql");
+//let CommonForMsSql = require("../../../../../DataSupply/Fs/LoginFolder/Admin/Setup/ForMsSql");
+let CommonForMsSql = require("../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/SetUp/FromTemplateDatas/ForSimpleAccounts/StartFuncs");
 
 let Basic = async ({ inUserPK }) => {
     return await CommonFromTemplate.StartFunc({ inUserPK });
@@ -14,7 +15,7 @@ let CreateFoldersOnly = async ({ inUserPK }) => {
 };
 
 let CreateForMsSql = async ({ inUserPK }) => {
-    return await CommonForMsSql.StartFunc({ inUserPK });
+    return await CommonForMsSql.StartFunc({ inDataPK: inUserPK });
 };
 
 module.exports = { Basic, CreateFoldersOnly, CreateForMsSql };
