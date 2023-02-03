c118f90e7efe291b4924f57ba6afaea425d5864b
Keshav <nknnkumar@yahoo.com>
2023-02-03 18:03:04 +0530

    keshav

--- Projects/JSONUser/Dal/Validate/Users/InUserGroupsJson/ForUserNamePasswordFirm.js
+++ Projects/JSONUser/Dal/Validate/Users/InUserGroupsJson/ForUserNamePasswordFirm.js
@@ -1,6 +1,6 @@
-// let DataSupply = require("../../../../../../DataSupply/Fs/LoginFolder/UserGroups/Find/InJson");
+let DataSupply = require("../../../../../../DataSupply/Fs/LoginFolder/UserGroups/Find/InJson");
 //let DataSupply = require("../../../../../../DataSupply/Fs/LoginFolder/UserGroups/InsertData/NewUser");
-let DataSupply = require("../../../../../../DataSupply/Fs/Config/JSONFolder/LoginFolder/UserGroupsJsonFile/Validate/ForUserPasswordFirm");
+let DataSupply1 = require("../../../../../../DataSupply/Fs/Config/JSONFolder/LoginFolder/UserGroupsJsonFile/Validate/ForUserPasswordFirm");
 
 let ForUserPasswordFirm = async ({ inUserName, inPassWord, inFirmName }) => {
     return await DataSupply.StartFunc({ inUserName, inPassWord, inFirmName });
