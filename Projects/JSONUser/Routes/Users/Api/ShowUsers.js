fe8b5ad704bcac89c68f6e726b3cec9375076455
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-03 21:51:25 +0530

    keshav

--- Projects/JSONUser/Routes/Users/Api/ShowUsers.js
+++ Projects/JSONUser/Routes/Users/Api/ShowUsers.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 let controllers = require("../../../controllers/Users/Api/ShowUsers.Controllers");
 
-
 router.get('/',controllers.ShowData);
 
 module.exports = router;
