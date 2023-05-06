8d0469d8a10afd39ec6203c4483ec9224ffcc38a
Keshav <nknnkumar@yahoo.com>
2023-05-06 13:49:26 +0530

    keshav

--- Projects/JSONUser/Routes/Login/Users/Admin/ShowUsers/WithFolderCheck.js
+++ Projects/JSONUser/Routes/Login/Users/Admin/ShowUsers/WithFolderCheck.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var router = express.Router();
 let controllers = require("../../../../../controllers/Login/Users/Admin/ShowUsers/WithFolderCheck.Controllers");
+const multer = require('multer');
+const decompress = require('decompress');
 
-router.get('/',controllers.FolderCheck);
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "");
+    },
+    filename: function (req, file, cb) {
+        console.log("file : ", file);
+        cb(null, file.originalname);
+    }
+});
+
+var upload = multer({ storage: storage });
+
+router.get('/', controllers.FolderCheck);
+router.post('/1', upload.single("inFile"), (req, res) => {
+    console.log("file : ", req.file.originalname);
+
+    decompress(req.file.originalname, 'dist').then(files => {
+        console.log('done!', files[0]);
+    });
+
+    res.end("");
+});
+
+router.post('/', upload.single("inFile"), controllers.PostFunc);
 
 module.exports = router;
--- Projects/JSONUser/controllers/Login/Users/Admin/ShowUsers/WithFolderCheck.Controllers.js
+++ Projects/JSONUser/controllers/Login/Users/Admin/ShowUsers/WithFolderCheck.Controllers.js
@@ -1,11 +1,22 @@
 let Repo = require("../../../../../Repository/Login/Users/Admin/ShowUsers/WithFolderCheck");
+const decompress = require('decompress');
 
 let FolderCheck = (req, res) => {
-
     Repo.FolderCheck({
     }).then((PromieData) => {
         res.end(JSON.stringify(PromieData));
     });
 };
 
-module.exports = { FolderCheck };
+let PostFunc = (req, res) => {
+    console.log("11111111111111 : ", req.file);
+
+    decompress(req.file.originalname, `dist/${req.file.fieldname}`).then(files => {
+        console.log('done!', files[0]);
+    });
+
+    res.end("");
+};
+
+
+module.exports = { FolderCheck, PostFunc };
