817b6db7f98dca0d0e43764468e6b3f4f9521b9e
keshavsoft <nknnkumar@yahoo.com>
2023-05-08 23:06:11 +0530

    keshav

--- Projects/JSONUser/controllers/Login/Users/Admin/InsertData/Controllers.FromUpload.js
+++ Projects/JSONUser/controllers/Login/Users/Admin/InsertData/Controllers.FromUpload.js
@@ -1,18 +1,13 @@
 let Repo = require("../../../../../Repository/Login/Users/Admin/InsertData/FromUpload");
 
 let Upload = (req, res) => {
-
-    // let LocalDataPk = req.KeshavSoft.DataPk;
-
     let LocalDataPk = req.params.inJsonPk;
-    console.log("kkkkkkkk", req.params, LocalDataPk);
+
     let LocalFromRepo = Repo.Upload({
         inDataPK: LocalDataPk
     });
-    console.log("LocalFromRepo", LocalFromRepo);
-    // .then((PromieData) => {
-    //     res.end(JSON.stringify(PromieData));
-    // });
+
+    res.json(LocalFromRepo);
 };
 
 module.exports = { Upload };
