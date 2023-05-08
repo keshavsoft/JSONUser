b67f151bf9cca7821d32e2200f0cedf4ea679886
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-08 20:44:12 +0530

    SREENIVAS

--- Projects/JSONUser/Routes/Login/Users/Admin.js
+++ Projects/JSONUser/Routes/Login/Users/Admin.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 let CommonShowUsers = require("./Admin/ShowUsers")
+let CommonInsertData = require("./Admin/InsertData")
 
 router.use('/ShowUsers', CommonShowUsers);
+router.use('/InsertData', CommonInsertData);
 
 module.exports = router;
