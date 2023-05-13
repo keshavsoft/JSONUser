3a82423069bab48456e34db2088998862184eab0
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-05-13 13:52:51 +0530

    ramp148

--- Projects/JSONUser/Middleware/Admin/Api/InAdminDataJson/Check.js
+++ Projects/JSONUser/Middleware/Admin/Api/InAdminDataJson/Check.js
@@ -1,7 +1,18 @@
-let CommonDataSupply = require("../../../../../../DataSupply/Fs/Users/AdminData/Check")
+//let LocalUserName = req.body.inUserName;
+//let LocalPassWord = req.body.inPassword;
+exports.LoginCheck = (req, res, next) => {
+    console.log("req.body", req.body);
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                inUserName: "",
+                inPassword: ""
+            },
+            originalUrl: req.originalUrl
+        });
+    } else {
 
-let ForUserAndPassword = async ({ inUserName, inPassWord, inoriginalUrl }) => {
-    return await CommonDataSupply.ForUserAndPassword({ inUserName, inPassWord, inoriginalUrl })
+    };
 };
-
-module.exports = { ForUserAndPassword };
