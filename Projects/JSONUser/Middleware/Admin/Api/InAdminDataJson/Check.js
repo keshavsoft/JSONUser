b7e9cfed6757f61e2df453f4c0df49350a8d3b73
Ramprasadnemala <ramprasadnemala12@gmail.com>
2023-05-13 16:53:54 +0530

    ramp486

--- Projects/JSONUser/Middleware/Admin/Api/InAdminDataJson/Check.js
+++ Projects/JSONUser/Middleware/Admin/Api/InAdminDataJson/Check.js
@@ -1,18 +1,26 @@
 //let LocalUserName = req.body.inUserName;
 //let LocalPassWord = req.body.inPassword;
 exports.LoginCheck = (req, res, next) => {
-    console.log("req.body", req.body);
     if (Object.keys(req.body).length === 0) {
         res.json({
             KTF: false,
             KReason: "post requst body should contain : ",
             body: {
-                inUserName: "",
-                inPassword: ""
-            },
-            originalUrl: req.originalUrl
+                inUserName:"",
+                inPassword:""
+            }
         });
-    } else {
 
+        return;
     };
+
+    if (("inUserName" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inUserName not found in body" })
+        return;
+    };
+    if (("inPassword" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inPassword not found in body" })
+        return;
+    };
+    next();
 };
