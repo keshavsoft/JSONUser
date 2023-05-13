4b311b2dec79ec36ca45b14df01a9d3d761a6a8e
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-13 16:53:43 +0530

    SREENIVAS

--- Projects/JSONUser/Dal/Admin/UserFolder/FromUpload/ShowDatas.js
+++ Projects/JSONUser/Dal/Admin/UserFolder/FromUpload/ShowDatas.js
@@ -1,8 +1,14 @@
 let CommonUserFuncs = require("../../../../../../DataSupply/Fs/Config/Compare/Upload-Json/ForPk");
+let CommonDataSupply = require("../../../../../../DataSupply/Fs/Config/JSONUploadFolder/UserFolders/DeleteFuncs/DeleteData");
 
 let ShowData = () => {
 
     return CommonUserFuncs.StartFunc();
 };
 
-module.exports = { ShowData };
+let Delete = ({inFolderName}) => {
+
+    return CommonDataSupply.StartFunc({inFolderName});
+};
+
+module.exports = { ShowData ,Delete};
