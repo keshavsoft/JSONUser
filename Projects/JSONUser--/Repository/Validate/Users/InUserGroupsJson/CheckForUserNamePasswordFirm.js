e5f0c59e5084817b61db93636d0f68af649382b2
keshavsoft <nknnkumar@yahoo.com>
2023-08-23 18:05:45 +0530

    keshav

--- Projects/JSONUser--/Repository/Validate/Users/InUserGroupsJson/CheckForUserNamePasswordFirm.js
+++
@@ -1,5 +0,0 @@
-let Dal = require("../../../../Dal/Validate/Users/InUserGroupsJson/CheckForUserNamePasswordFirm")
-
-exports.ForUserPasswordFirm = async ({ inUserName, inPassWord, inFirmName }) => {
-   return await Dal.ForUserPasswordFirm({ inUserName, inPassWord, inFirmName });
-};
